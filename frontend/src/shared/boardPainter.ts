eee1009cf34e33095cf5a0a0a9c234654b2781a5
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-09-26 14:08:42 +0900

    add board dots

--- frontend/src/shared/boardPainter.ts
+++ frontend/src/shared/boardPainter.ts
@@ -1,4 +1,4 @@
-import { BOARD_BACKGROUND, INNER_MARGIN, LINE_COUNT, LINE_THICKNESS, Point } from "./boardSetting";
+import { BOARD_BACKGROUND, INNER_MARGIN, LINE_COUNT, LINE_THICKNESS, Point, DOT_RADIUS } from "./boardSetting";
 
 export function drawBoardBase(ctx: CanvasRenderingContext2D, width: number, height: number) {
   ctx.fillStyle = BOARD_BACKGROUND;
@@ -29,6 +29,20 @@ export function drawBoardBase(ctx: CanvasRenderingContext2D, width: number, heig
       LINE_THICKNESS * height
     );
   }
+
+  // draw dots
+  // 3, 9, 15
+
+  for (let i = 0; i < LINE_COUNT; i++) {
+    for (let j = 0; j < LINE_COUNT; j++) {
+      if (i%6 === 3 && j%6 === 3) {
+        let x = leftTop[0] + i * horInterval;
+        let y = leftTop[1] + j * verInterval;
+        drawDot(ctx, x, y);
+      }
+    }
+  }
+  
 }
 
 export function drawLine(ctx: CanvasRenderingContext2D, from: Point, to: Point, thickness: number) {
@@ -43,5 +57,12 @@ export function drawCircle(ctx: CanvasRenderingContext2D, x: number, y: number, 
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.fillStyle = color;
+  ctx.fill();
+}
+
+export function drawDot(ctx: CanvasRenderingContext2D, x: number, y: number) {
+  ctx.beginPath();
+  ctx.arc(x, y, DOT_RADIUS, 0, 2 * Math.PI);
+  ctx.fillStyle = 'black';
   ctx.fill();
 }
--- frontend/src/shared/boardSetting.ts
+++ frontend/src/shared/boardSetting.ts
@@ -2,6 +2,7 @@ export const BOARD_BACKGROUND = "rgb(211, 168, 74)";
 export const LINE_COUNT = 19;
 export const INNER_MARGIN = 0.05;
 export const LINE_THICKNESS = 0.003;
+export const DOT_RADIUS = 4;
 export const BOARD_WIDTH = 600;
 export const BOARD_HEIGHT = 600;
 export const PIECE_RADIUS = 15;
